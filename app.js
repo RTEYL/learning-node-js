62455578df39db2c50b656f619a0374adfbdb4ee
RTEYL <rteyl1690@gmail.com>
2021-02-07 14:09:59 -0800

    modularize

--- app.js
+++ app.js
@@ -1,37 +1,7 @@
 const http = require('http')
-const fs = require('fs')
-const { runInNewContext } = require('vm')
+
 const server = http.createServer((req, res) => {
-  const {url,method} = req
 
-  if(url === '/'){
-    res.write('<html>')
-    res.write('<head><title>Form</title></head>')
-    res.write('<body><form action="/message" method="POST"><input type="text" name="message"/><button type="submit">Send</button></form></body>')
-    res.write('</html>')
-    return res.end()
-  }
-
-  if(url === '/message' && method ==='POST' ){
-    const body = []
-    req.on('data', (chunk) =>{
-      body.push(chunk)
-    })
-    req.on('end', () => {
-      const parsedBody = Buffer.concat(body).toString()
-      const message = parsedBody.split('=')[1]
-      fs.writeFile('message.txt', message, (err) =>{
-        res.writeHead(302, {'Location': '/'})
-        return res.end()
-      })
-    })
-  }
-
-  res.setHeader('Content-Type', 'text/html')
-  res.write('<html>')
-  res.write('<head><title>My First Page</title></head>')
-  res.write('</html>')
-  res.end()
 
 })
 
