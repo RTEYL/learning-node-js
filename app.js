34743d7b26bb0bb1885f41b656b4bf773a82644c
RTEYL <rteyl1690@gmail.com>
2021-02-07 13:47:30 -0800

    use async write file

--- app.js
+++ app.js
@@ -20,10 +20,11 @@ const server = http.createServer((req, res) => {
     req.on('end', () => {
       const parsedBody = Buffer.concat(body).toString()
       const message = parsedBody.split('=')[1]
-      fs.writeFileSync('message.txt', message)
+      fs.writeFile('message.txt', message, (err) =>{
+        res.writeHead(302, {'Location': '/'})
+        return res.end()
+      })
     })
-    res.writeHead(302, {'Location': '/'})
-    return res.end()
   }
 
   res.setHeader('Content-Type', 'text/html')
