3ad8d3a8c559c1274ef1875cac5f8ae6dbf63d56
RTEYL <rteyl1690@gmail.com>
2021-02-09 13:30:46 -0800

    use body parser

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express()
 
-app.use('/', (req, res, next) => {
-  console.log('always runs');
-  next()
-})
+app.use(bodyParser.urlencoded({extended: false}))
 
 app.use('/add-product', (req, res, next) => {
-  res.send('Add Product')
+  res.send('<form action="/product" method="POST"><input type="text" name="title"/><button type"submit">Submit</button></form>')
+})
+
+app.post('/product', (req, res, next) => {
+  console.log(req.body)
+  res.redirect('/')
 })
 
 app.use('/', (req, res, next) => {
