fcbbdea3363dd36135375935404114483a564a81
RTEYL <rteyl1690@gmail.com>
2021-02-09 12:59:10 -0800

    try routes

--- app.js
+++ app.js
@@ -2,9 +2,17 @@ const express = require('express')
 
 const app = express()
 
-app.use((req, res, next) => {
-  res.send()
+app.use('/', (req, res, next) => {
+  console.log('always runs');
   next()
+})
+
+app.use('/add-product', (req, res, next) => {
+  res.send('Add Product')
+})
+
+app.use('/', (req, res, next) => {
+  res.send('Home Page')
 })
 
 app.listen(3000)
