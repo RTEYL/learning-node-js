05bcaf11165a5d5636d84ac308311adb7bf2fa58
RTEYL <rteyl1690@gmail.com>
2021-02-10 17:11:46 -0800

    change inline style to public css

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
-
+const path = require('path')
 const app = express()
 
 app.use(bodyParser.urlencoded({extended: false}))
+
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/admin', adminRoutes)
 
