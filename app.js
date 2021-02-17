c6f7b1c994da3ff92f88e613d2acb125b7911200
RTEYL <rteyl1690@gmail.com>
2021-02-17 14:51:04 -0800

    implement ejs

--- app.js
+++ app.js
@@ -3,14 +3,17 @@ const bodyParser = require('body-parser')
 const adminData = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 const path = require('path')
-const expressHbs = require('express-handlebars')
+// const expressHbs = require('express-handlebars')
 
 const app = express()
 
-app.engine('hbs', expressHbs({layoutsDir: 'views/layouts', defaultLayout: 'main-layout', extname: 'hbs'}))
+
+
+// app.engine('hbs', expressHbs({layoutsDir: 'views/layouts', defaultLayout: 'main-layout', extname: 'hbs'}))
 
 // app.set('view engine', 'pug')
-app.set('view engine', 'hbs')
+// app.set('view engine', 'hbs')
+app.set('view engine', 'ejs')
 app.set('views', 'views')
 
 app.use(bodyParser.urlencoded({extended: false}))
