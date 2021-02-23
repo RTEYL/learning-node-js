8ea44a6a5362f4a046fa7d97f33d0205eb186e11
RTEYL <rteyl1690@gmail.com>
2021-02-22 18:09:04 -0800

    add comment to default express config

--- app.js
+++ app.js
@@ -14,8 +14,12 @@ const app = express()
 // app.set('view engine', 'pug')
 // app.set('view engine', 'hbs')
 app.set('view engine', 'ejs')
-app.set('views', 'views')
 
+//default setting
+
+  // app.set('views', 'views')
+
+//
 app.use(bodyParser.urlencoded({extended: false}))
 
 app.use(express.static(path.join(__dirname, 'public')))
